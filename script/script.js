c7a70c78bb1d958ce0b87683de3bbc2faef32f8d
ClayZz-dev <nori.bungkusnasi@gmail.com>
2021-04-06 17:34:57 +0700

    removing done box and adding delete option

--- script/script.js
+++ script/script.js
@@ -20,8 +20,9 @@ const todoListUl = document.querySelectorAll(".todo-list");
 
 let todoArr = [];
 
-let LOCAL_STORAGE_KEY = "todo.id";
-let todos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY, )) || [];
+let LOCAL_STORAGE_KEY = "todo.name";
+let LOCAL_STORAGE_ID_KEY = "todo.id";
+let todos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY,)) || [];
 
 let idNumber = 0;
 let allIndex = 0;
@@ -34,7 +35,10 @@ let doneIndex = 2;
 // const filterDone = document.querySelector("#done");
 
 
-addBtn.addEventListener("click", createTodo);
+addBtn.addEventListener("click", () => {
+    createTodo();
+    inputbar.value = "";
+});
 inputbar.addEventListener("keypress", e => {
     if (e.keyCode == 13) {
         createTodo();
@@ -50,19 +54,47 @@ inputbar.addEventListener("keypress", e => {
 // });
 window.addEventListener("DOMContentLoaded", showingLocalStorageTodo);
 todoListUl[allIndex].addEventListener("click", checkMark);
-todoListUl[allIndex].addEventListener("click", dotsOption);
+todoListUl[allIndex].addEventListener("click", e => {
+    const target = e.target;
+    const parent = target.parentElement;
+    const selected = parent.parentElement;
 
+    if(target.classList.contains("dots")){
+        parent.nextSibling.classList.toggle("clicked");
+    }
+    else if(target.classList.contains("delete-todo")){
+        selected.parentElement.style.display = "none";
+        clearTodo(selected.parentElement.innerText);
+    }
+    console.log(target)
+    console.log(todoArr)
+  
+    // todoArr.forEach(todo => {
+    //     const optionBox = document.querySelector(".option-dots").dataset.dotId;
+    //     const box = document.querySelector(`[data-dot-id]`)
+    //     if (optionBox == selected.dataset.todoId) {
+    //         console.log(optionBox)
+    //     } else return "mantap"
+    //     console.log(todo)
+    // })
+   
+    // // console.log(target)
+    // // console.log(parent)
+    // // console.log(selected)
+});
+
+
+function generateNumber(todoName) {
+    let random = Math.random(11);
+
+    const num = todoName.length;
+
+    return num * Math.round(random) + Math.random();
+}
 // todoListUl[allIndex].addEventListener("click", deleteTodo);
 
-function render(){
-    todoArr.forEach(todos => {
-
-    })
-}
-
-
 function createTodo() {
-    if(inputbar.value == null || inputbar.value === "") return;
+    if (inputbar.value == null || inputbar.value === "") return;
     // creating li container with class of todo
     const todo = document.createElement("li");
     todo.classList.add("todo");
@@ -70,9 +102,9 @@ function createTodo() {
 
     const todoName = inputbar.value;
     const newTodo = saveTodo(todoName);
-    todoArr.push(newTodo);  
+    saveToLocalStorage(todoName)
+    todoArr.push(newTodo);
     todo.dataset.todoId = newTodo.id;
-    console.log(newTodo.id)
 
     // creating CHECKMARK button
     const checkIconBtn = document.createElement("button");
@@ -92,6 +124,7 @@ function createTodo() {
     // creting option when dots is cliced
     const optionDots = document.createElement("div");
     optionDots.classList.add("option-dots");
+    optionDots.dataset.dotId = newTodo.id;
     const optContainer = document.createElement("ul");
     optContainer.classList.add("option-container");
 
@@ -115,66 +148,74 @@ function createTodo() {
 }
 
 
-function saveTodo(todoName){
-    if(localStorage.getItem(LOCAL_STORAGE_KEY) === null){
-        todos = []    
+function saveTodo(todoName) {
+    return { id: generateNumber(todoName), name: todoName, completeStatus: false }
+}
+
+function saveToLocalStorage(todo) {
+    if (localStorage.getItem(LOCAL_STORAGE_KEY) === null) {
+        todos = []
     } else {
         todos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
     }
-    todos.push(todoName);
+    todos.push(todo);
     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
-    return {id : Date.now().toString(), name: todoName, completeStatus: false}
 }
 
-function showingLocalStorageTodo(){
+function showingLocalStorageTodo() {
     let todos;
-    if(localStorage.getItem(LOCAL_STORAGE_KEY) === null){
-        todos = []    
+    if (localStorage.getItem(LOCAL_STORAGE_KEY) === null) {
+        todos = []
     } else {
         todos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
-    } 
+    }
+    // *if there's item in local storage, add it right away
     todos.forEach(list => {
         const todo = document.createElement("li");
         todo.classList.add("todo");
-        todo.dataset.todoId;
         todo.setAttribute("draggable", "true");
-    
+
+        const todoName = list;
+        const newTodo = saveTodo(todoName);
+        todoArr.push(newTodo);
+        todo.dataset.todoId = newTodo.id;
         // creating CHECKMARK button
         const checkIconBtn = document.createElement("button");
         checkIconBtn.innerHTML = `<i class="far fa-square icon"></i>`;
-    
+
         // creating the text of the todo
         const todoText = document.createElement("div");
         todoText.classList.add("todo-text");
-    
-        // the text is based on user input value
+
+        // the text is based on user value in local storage
         todoText.innerHTML = `<span>${list}</span>`;
-    
+
         // creating dots icon
         const dotsIconBtn = document.createElement("button");
         dotsIconBtn.innerHTML = `<i class="fas fa-ellipsis-h dots"></i>`;
-    
+
         // creting option when dots is cliced
         const optionDots = document.createElement("div");
         optionDots.classList.add("option-dots");
+        optionDots.dataset.dotId = newTodo.id;
         const optContainer = document.createElement("ul");
         optContainer.classList.add("option-container");
-    
+
         // delete option
         const optionList = document.createElement("li");
         optionList.innerText = "Delete";
         optionList.classList.add("delete-todo");
-    
+
         // append child to parent element
         optContainer.appendChild(optionList);
         optionDots.appendChild(optContainer);
-    
-    
+
+
         todo.appendChild(checkIconBtn);
         todo.appendChild(todoText);
         todo.appendChild(dotsIconBtn);
         todo.appendChild(optionDots);
-    
+
         todoListUl[allIndex].appendChild(todo);
     })
 }
@@ -184,7 +225,7 @@ function checkMark(e) {
     const todo = e.target.parentElement;
     const target = e.target;
 
-    if(target.classList.contains("icon")){
+    if (target.classList.contains("icon")) {
         todo.parentElement.classList.toggle("checked");
         target.classList.toggle("fa-square");
         target.classList.toggle("fa-check-square");
@@ -193,15 +234,22 @@ function checkMark(e) {
     }
 }
 
-
-
-function dotsOption(e) {
-  
+function clearTodo(todo){
+    if (localStorage.getItem(LOCAL_STORAGE_KEY) === null) {
+        todos = []
+    } else {
+        todos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    }
+    todos.splice(todo, 1)
+    todoArr.splice(todo, 1);
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
 }
 
-function deleteTodo(e) {
+// function dotsOption(e) {
+//     const optionBox = document.querySelector(".option-dots")
+//     if(e.)
+// }
 
-}
 
 
 
@@ -234,13 +282,3 @@ function deleteTodo(e) {
 // dotsButton.forEach(e => {
 //     e.addEventListener("click", dotsOption);
 // })
-
-// function dotsOption(){
-//     for(let i = 0; i < dotsButton.length; i++){
-//         optionBox[i].onclick = () => {
-//             this.classList.toggle("clicked");
-//         }
-
-//         console.log(this);
-//     }
-// }
